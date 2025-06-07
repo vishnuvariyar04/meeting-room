a20d102a4a7c9c0b64496e97f4a28e4d37e6b01e
vishnu variyar <vishnuvariyar04@gmail.com>
2025-06-08 01:14:59 +0530

    small changes

--- src/app/forgot-password/page.js
+++ src/app/forgot-password/page.js
@@ -61,7 +61,7 @@ export default function ForgotPassword() {
             Reset Password
           </h2>
           <p className="mt-2 text-sm text-gray-600">
-            Enter your email address and we'll send you instructions to reset your password.
+            Enter your email address and we&apos;ll send you instructions to reset your password.
           </p>
         </div>
       </div>
